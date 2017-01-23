d3c19c72e57d46c51f7008f8a0e04cb2fab08afa
Ian Gomez <ian.nicolasgomez@gmail.com>
2017-01-22 16:29:01 -0800

    tryna fix things

---
+++ Trajectory_Sims/motor_impulse_extrator.m
@@ -0,0 +1,30 @@
+% Motor Impulse Extractor
+% Ian Gomez
+% 1/22/17 
+
+%% Data Import
+
+cd old_flight_data
+filename = '2016-05-21_DairyAire_Firestorm_H999_flattened.csv';
+data = xlsread(filename);
+cd ..
+
+t = data(:,1); % s
+axial_accel = data(:,2); % Gs
+T = data(:,6); % F
+vel = data(:,7); % ft/s
+alt_a = data(:,13); % ft, based on accelerometer 
+alt_b_AGL = data(:,14); % ft, AGL (based on barometer)
+alt_b_ASL = data(:, 15); % ft, ASL (based on barometer)
+
+% Find the end of flight
+for i = 0:length(t)
+   if  
+end
+
+%% Plotting
+clc; close all
+
+figure(1)
+plot(t,vel)
+xlim([0,
