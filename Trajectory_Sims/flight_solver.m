c392b276f6d5dfbdc8332111c9c276d4148afbd9
Ian Gomez <ian.nicolasgomez@gmail.com>
2017-02-13 12:03:56 -0800

    First draft of solver

---
+++ Trajectory_Sims/flight_solver.m
@@ -0,0 +1,19 @@
+
+ft2in = 12; % in/ft
+
+% inputs:
+rocket.m = NaN;          % kg, mass
+rocket.l = NaN * ft2in;  % ft, length
+rocket.d = NaN;          % in, diameter
+rocket.fin;              % fin properties
+
+rocket.parachute.d = NaN;
+rocket.parachute.cd = NaN;
+rocket.drogue.d = NaN;
+rocket.drogue.cd = NaN;
+
+% atmosphere model
+% wind profile 
+% gust modeling
+% current speed, current mass, current acceleration, angle of attack
+
