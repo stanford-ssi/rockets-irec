f90f2c3f5e41776f59301f1fdb03913af8135942
Ian Gomez <ian.nicolasgomez@gmail.com>
2017-03-05 17:36:06 -0800

    set up the simulation

--- Drag_Fin/getAtmoConditions.m
+++ Drag_Fin/getAtmoConditions.m
@@ -53,13 +53,13 @@ else
 end
 
 % Pressure [Pa]
-g = 9.81; % m/s^2
+g = 9.81;                   % m/s^2
 avogadro = 6.0221409e23;
 k_boltz = 1.38064852e-23;   % J/K
 m_air   = 29/avogadro*1e-3; % kg
 R_air =  287;               % J/kg K
-P_sl  = 101300;          % Pa, sea level pressure 
-k = 1.4;          % Air specific heats ratio
+P_sl  = 101300;             % Pa, sea level pressure 
+k = 1.4;                    % Air specific heats ratio
 P = P_sl.*exp((-m_air.*g.*h)./(k_boltz.*T)); % Pa
 
 % Density [kg/m^3]
