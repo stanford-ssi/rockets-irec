4c779264a6970336e6c9066e42a1125a8ea256fe
Rebecca Wong <rebeccawong@stanford.edu>
2017-05-01 20:51:20 -0700

    added LCD display for RSSI indicator

--- Avionics/XBee_RSSI_Status/XBee_RSSI_Status.ino
+++ Avionics/XBee_RSSI_Status/XBee_RSSI_Status.ino
@@ -1,31 +1,54 @@
+/*
+ * XBee RSSI Status Display
+ * 
+ * Stanford SSI Rockets IREC 2017
+ * R.Wong
+ * 
+ * rev 1.0 2017-05-01
+ * 
+ * LCD Display Circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ */
+
 #include <SoftwareSerial.h>
-//#include <XBee.h>
+#include <LiquidCrystal.h>
 
-int xBeeRX = 2; // Connect Arduino pin 8 to Xbee TX pin
-int xBeeTX = 3; // Connect Arduino pin 9 to Xbee RX pin
-int rssi = 12; // RSSI digitalPin
+int xBeeRX = 7; // Connect Arduino pin 8 to Xbee TX pin
+int xBeeTX = 8; // Connect Arduino pin 9 to Xbee RX pin
+int rssi = 9; // RSSI digitalPin
 int ledPin = 13; //LED
+
 SoftwareSerial xBeeSerial(xBeeRX, xBeeTX);// Define SoftwareSerial xBeeSerial RX/TX pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-char incomingByte, checkByte;
 unsigned long rssiDur;
-
-//XBee xbee=XBee();
 
 void setup() {
   // initialize both serial ports:
   Serial.begin(115200);
   xBeeSerial.begin(115200);
-//  xbee.setSerial(xBeeSerial);
+  lcd.begin(16, 2);
   pinMode(rssi, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   rssiDur = pulseIn(rssi, LOW);
-  Serial.print(" Strength: ");
-  Serial.print(rssiDur);
-  Serial.println();
+//  Serial.print(" Strength: ");
+//  Serial.print(rssiDur);
+//  Serial.println();
+  lcd.clear();
+  lcd.print(" Strength: ");
+  lcd.print(rssiDur);
   digitalWrite(ledPin, HIGH);
   delay(50);
   digitalWrite(ledPin, LOW);
