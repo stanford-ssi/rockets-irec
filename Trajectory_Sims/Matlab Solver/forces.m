462359a6b22557ab02bb5ff995b36d1404d56248
iangomez <ian.nicolasgomez@gmail.com>
2017-05-17 04:29:21 -0700

    Getting better all the time. Aerodynamics is looking better.

--- Trajectory_Sims/Matlab Solver/forces.m
+++ Trajectory_Sims/Matlab Solver/forces.m
@@ -10,9 +10,10 @@
 
 % Split each force into x and y for earth centered components
 
-function [f_x, f_y, moment, Fdrag, Flift, CD, CL, aoa, gravity] = ...
+function [f_x, f_y, moment, Fdrag, Flift, CD, CL, aoa, gravity, CP] = ...
     forces(t, t_step, r, u, T, current_mass, wind, aerodata, rocket, CM)
 
+ux = u(1);
 theta = r(3); % wrt to the vertical (normal to the earth's surface)
 
 % thrust
@@ -50,22 +51,29 @@ Dy = cosd(theta)*Fdrag;
 G = 3.986E14; r_earth = 6378000;
 gravity = (G)/(r_earth + norm(r(1:2))).^2 * current_mass;
 
+% no gravity at the ground
 site_elevation = 1293;
-if r(2) <= site_elevation
+if r(2) == site_elevation
     gravity = 0;
 end
 
-if wind >= 0; dir = 1; else dir = -1; end % change direction of wind
+if wind >= ux; dir = 1; else dir = -1; end % change direction of wind
 
 % Requires CM and CP distance from bottom of the rocket
 if u(2) >= 0
     f_x = Tx + Lx + dir*Dx;
     f_y = Ty + Ly - Dy  - gravity;
     moment = (Lx - Dx)*cosd(theta)*(CP-CM) + (Dy + Ly)*sind(theta)*(CP-CM);
-else
-    f_x = dir*Dx;
-    f_y = Dy - gravity;
-    moment = 0; %not worth it
+%     moment = (Lx - Dx)*(CP-CM) + (Dy + Ly)*(CP-CM);
+
+else    
+    f_x = Dx + dir*Lx;
+    f_y = Ly + Dy - gravity;
+    moment = 0; % not worth it, rocket would be swinging
 end
+
+% if r(2) < site_elevation
+%     fx = 0; fy = 0; moment = 0;
+% end
 
 end
--- Trajectory_Sims/Matlab Solver/initialconditions.m
+++ Trajectory_Sims/Matlab Solver/initialconditions.m
@@ -2,6 +2,8 @@
 % These items don't need to run every single time the simulation is run
 % Ian Gomez, May 16, 2017
 
+m2mi = 0.000621371;
+in2m = 0.0254;
 m2ft = 3.28084;
 lbs2N = 4.44822162825;
 
@@ -11,7 +13,9 @@ motor.name  = 'N2900';
 % Local site conditions and make rocket struct
 % Note: measure CP and CM from nose of rocket
 site_elevation = NM; % m
-goal = site_elevation + 9144;
+goal = 9144;
+goalkft = 30e3; % ft
+band = 2e3; % ft
 
 % Set up simulation
 t_step = 0.01; % for motor data interpolation
@@ -24,7 +28,7 @@ T = [(0:t_step:motor.burntime)', T'];  % make thrust a paired vector
 
 % Pull RASAero aerodynamics data
 csvnum1 = 2500; csvnum2 = csvnum1*2;
-RASdata = csvread('RASAero_N2900_aerodata.csv', 1, 0);
+RASdata = csvread('RASAero_N2900_bestweather_aerodata.csv', 1, 0);
 RASMa = RASdata(1:csvnum1,1);
 CD0 = RASdata(1:csvnum1,3);
 CD2 = RASdata(csvnum1+1:csvnum2,3);
@@ -35,7 +39,8 @@ CL4 = RASdata(csvnum2:end,8);
 CP0 = RASdata(1:csvnum1,13);
 CP2 = RASdata(csvnum1+1:csvnum2,13);
 CP4 = RASdata(csvnum2:end,13);
-aerodata = [zeros(1,10); RASMa, CD0, CD2, CD4, CL0, CL2, CL4, CP0, CP2, CP4];
+aerodata = [zeros(1,10); RASMa, CD0, CD2, CD4, CL0, CL2, CL4,...
+    CP0*in2m, CP2*in2m, CP4*in2m];
 clearvars csvnum1 csvnum2 RASdata RASMa CD0 CD2 CD4 CL0 CL2 CL4
 
 % Pull RASAero flight data
@@ -47,5 +52,10 @@ ras_T   = RASFlight(:,6).*lbs2N; % lb, thrust
 ras_W   = RASFlight(:,7).*lbs2N; % lb, weight
 ras_D   = RASFlight(:,8).*lbs2N; % lb, drag
 ras_L   = RASFlight(:,9).*lbs2N; % lb, lift
+ras_CM  = RASFlight(:,10);       % in from nose
+ras_CP  = RASFlight(:,11);       % in from nose
 ras_alt = RASFlight(:,21);       % ft, altitude
-ras_v   = RASFlight(:,16);       % ft, velocity
+ras_v   = RASFlight(:,16);       % ft/s, velocity mag
+ras_vy  = RASFlight(:,17);       % ft/s
+ras_vx  = RASFlight(:,18);       % ft/s
+ras_theta = RASFlight(:,20);     % degrees, path angle
